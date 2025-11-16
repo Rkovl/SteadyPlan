666ec429ae04b29f15337eebacf54949804893fb
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-15 20:36:15 -0800

    removed the head from header

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Partial Test</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/stylesheet.css">
-  </head>
+
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom container"> 
             <div class="col-md-3 mb-2 mb-md-0"> 
                 <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none align-items-center fw-bold"> 

af47cfba0c1bec9978b2db131a554c192b88e710
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-15 17:28:39 -0800

    basic temp partial

--- partials/header.php
+++ partials/header.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Partial Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
-  <body>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"> 
         <div class="col-md-3 mb-2 mb-md-0"> 
             <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none"> 
---
+++ public/partialTest.php
@@ -0,0 +1,7 @@
+<?php
+include __DIR__.'/../partials/header.php';
+?>
+
+<?php
+include __DIR__.'/../partials/footer.php';
+?>

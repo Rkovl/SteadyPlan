8a0085e0a2aa157921faac9b4b9e54474ace3580
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-16 19:11:34 -0800

    sticky navbar

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,7 @@
     $isLoggedIn = isLoggedIn();
     //$isLoggedIn = true; //for testing purposes
 ?>
-<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom container">
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom container sticky-top">
     <div class="col-md-3 mb-2 mb-md-0 col-6">
         <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none align-items-center fw-bold">
             <img src="./images/SteadyPlan_Logo.png" alt="Logo" class="bi me-2" width="40" height="40">

3a2b8e7f0da3ccf7ba169893f5be8a35e95f7220
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-19 20:37:24 -0800

    made basic landing page
    replace example page with basic landing page

--- public/index.php
+++ public/index.php
@@ -2,7 +2,38 @@
 include __DIR__.'/../partials/defaultHead.php';
 include __DIR__.'/../partials/header.php';
 ?>
-
+<style>
+    body {
+          background: linear-gradient(165deg, 
+        #9fc8f7ff 50%,
+        white 50%);
+        min-height: 100vh;
+    }
+</style>
+<div class="container">
+    <div class="col">
+        <div class="row">
+            <div class="col d-flex flex-column justify-content-center align-items-start">
+                <h1 class="fw-bold">Steady Plan — Where tasks stay clear and progress stays steady.</h1>
+                <p class="pt-3 pb-3 pe-5 me-5">Steady Plan is a collaborative project management platform designed to keep teams organized and aligned. Create projects, track tasks, assign responsibilities, and visualize progress through an intuitive Kanban workflow. Built to support clear communication, structured teamwork, and consistent project delivery.</p>
+                <button class="btn btn-primary d-inline-flex align-items-center" type="button">Sign-up Today →</button>
+            </div>
+            <div class="col-1"></div>
+            <div class="col">
+                <img src="./images/tempShowcaseImage.png" alt="Showcase Image" class="img-fluid shadow">
+            </div>
+        </div>
+        <div class="pt-5 pb-5 mt-5 mb-5">
+        </div>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3"> <div class="feature col"> <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"> <svg class="bi" width="1em" height="1em" aria-hidden="true"><use xlink:href="#collection"></use></svg> </div> <h3 class="fs-2 text-body-emphasis">Featured title</h3> <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+Call to action
+<svg class="bi" aria-hidden="true"><use xlink:href="#chevron-right"></use></svg> </a> </div> <div class="feature col"> <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"> <svg class="bi" width="1em" height="1em" aria-hidden="true"><use xlink:href="#people-circle"></use></svg> </div> <h3 class="fs-2 text-body-emphasis">Featured title</h3> <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+Call to action
+<svg class="bi" aria-hidden="true"><use xlink:href="#chevron-right"></use></svg> </a> </div> <div class="feature col"> <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"> <svg class="bi" width="1em" height="1em" aria-hidden="true"><use xlink:href="#toggles2"></use></svg> </div> <h3 class="fs-2 text-body-emphasis">Featured title</h3> <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+Call to action
+<svg class="bi" aria-hidden="true"><use xlink:href="#chevron-right"></use></svg> </a> </div> </div>
+    </div>
+</div>
 <?php
 include __DIR__.'/../partials/footer.php';
 ?>

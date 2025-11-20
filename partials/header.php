8922ebeaf4be72f1a0c1d829f4006b3fd837e7d6
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-19 21:31:07 -0800

    improved landing page
    add icons and description to features, made top half of page responsive

--- partials/header.php
+++ partials/header.php
@@ -3,25 +3,25 @@
     $isLoggedIn = isLoggedIn();
     //$isLoggedIn = true; //for testing purposes
 ?>
-<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom container sticky-top">
-    <div class="col-md-3 mb-2 mb-md-0 col-6">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none align-items-center fw-bold">
-            <img src="./images/SteadyPlan_Logo.png" alt="Logo" class="bi me-2" width="40" height="40">
-            Steady Plan
-        </a>
-    </div>
+<header class="navbar navbar-expand-md sticky-top mb-4 ">
+    <nav class="container border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0 col-6">
+            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none align-items-center fw-bold">
+                <img src="./images/SteadyPlan_Logo.png" alt="Logo" class="bi me-2" width="40" height="40">
+                Steady Plan
+            </a>
+        </div>
 <?php if ($isLoggedIn): ?>
-    <div class="col-md-4 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Dashboard</button>
-        <button type="button" class="btn btn-primary">Logout</button>
-    </div>
+        <div class="col-md-4 text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Dashboard</button>
+            <button type="button" class="btn btn-primary">Logout</button>
+        </div>
 
 <?php else: ?>
-    <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
-    </div>
-</header>
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
 <?php endif; ?>
-
+    </nav>
 </header>

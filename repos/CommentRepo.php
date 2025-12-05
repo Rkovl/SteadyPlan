a4903b2175d1fbaf018ae3596a0993752646899b
Daniel <102389559+UtterEvergreen1@users.noreply.github.com>
2025-12-04 18:53:57 -0800

    Changed CommentRepo to use static db

--- repos/CommentRepo.php
+++ repos/CommentRepo.php
@@ -1,16 +1,11 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/db/database.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/repos/BaseRepo.php');
 
-class CommentRepo {
-    private $db;
-
-    function __construct() {
-        $this->db = Database::getInstance()->getConnection();
-    }
-
+class CommentRepo extends BaseRepo {
     public function addComment($task_id, $user_owner, $content) {
         $query = 'INSERT INTO comments (task_id, user_owner, content) VALUES (:task_id, :owner, :body)';
-        $stmt = $this->db->prepare($query);
+        $stmt = BaseRepo::getDB()->prepare($query);
         $stmt->bindParam(':task_id', $task_id);
         $stmt->bindParam(':owner', $user_owner);
         $stmt->bindParam(':body', $content);
@@ -25,7 +20,7 @@ class CommentRepo {
 
     public function editComment($comment_id,  $content) {
         $query = 'UPDATE comments SET content = :content WHERE id = :id';
-        $stmt = $this->db->prepare($query);
+        $stmt = BaseRepo::getDB()->prepare($query);
         $stmt->bindParam(':content', $content);
         $stmt->bindParam(':id', $comment_id);
         return $stmt->execute();
@@ -33,14 +28,14 @@ class CommentRepo {
 
     public function deleteComment($comment_id) {
         $query = 'DELETE FROM comments WHERE id = :id';
-        $stmt = $this->db->prepare($query);
+        $stmt = BaseRepo::getDB()->prepare($query);
         $stmt->bindParam(':id', $comment_id);
         return $stmt->execute();
     }
 
     public function getTaskComments($task_id) {
         $query = 'SELECT * FROM comments WHERE task_id = :task_id';
-        $stmt = $this->db->prepare($query);
+        $stmt = BaseRepo::getDB()->prepare($query);
         $stmt->bindParam(':task_id', $task_id);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

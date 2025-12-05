aa2700633e67aa0201b7a6304555283433633726
Jonathan Wirick <questionablly@gmail.com>
2025-12-04 20:29:06 -0800

    tokensrepo fixed

--- db/auth.php
+++ db/auth.php
@@ -2,7 +2,8 @@
 session_start();
 
 function isLoggedIn() {
-    return isset($_SESSION['user_id']);
+//    return isset($_SESSION['user_id']);
+    if(isset($_SESSION['login_cookie'])) {}
 }
 
 ?>
--- repos/TokensRepo.php
+++ repos/TokensRepo.php
@@ -5,20 +5,20 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/repos/BaseRepo.php');
 
 class TokensRepo extends BaseRepo
 {
-    public function addToken($user_id, $token, $date) {
+    public static function addToken($user_id, $token, $date) {
         $query = "INSERT INTO tokens (user_id, token, date) VALUES (:user_id, :token, :date)";
         $stmt = BaseRepo::getDB()->prepare($query);
-        $stmt->bindParam(':userID', $user_id);
+        $stmt->bindParam(':user_id', $user_id);
         $stmt->bindParam(':token', $token);
         $stmt->bindParam(':date', $date);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function isTokenValid($user_id, $token) {
-        $query = "SELECT date FROM tokens WHERE user_id = :userID AND token = :token";
+    public static function isTokenValid($user_id, $token) {
+        $query = "SELECT date FROM tokens WHERE user_id = :user_id AND token = :token";
         $stmt = BaseRepo::getDB()->prepare($query);
-        $stmt->bindParam(':userID', $user_id);
+        $stmt->bindParam(':user_id', $user_id);
         $stmt->bindParam(':token', $token);
         $stmt->execute();
 
@@ -26,9 +26,9 @@ class TokensRepo extends BaseRepo
         $currentTime = time();
 
         if($currentTime > $time) {
-            $query2 = "DELETE FROM tokens WHERE user_id = :userID AND token = :token";
+            $query2 = "DELETE FROM tokens WHERE user_id = :user_id AND token = :token";
             $stmt2 = BaseRepo::getDB()->prepare($query2);
-            $stmt2->bindParam(':userID', $user_id);
+            $stmt2->bindParam(':user_id', $user_id);
             $stmt2->bindParam(':token', $token);
             $stmt2->execute();
             return false;
